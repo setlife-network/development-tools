4eccc1fa21766acda852ac016e1184901daf10dc
Oscar Lafarga <otech47@gmail.com>
2018-04-23 07:03:27 -0400

    clear

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,16 +7,10 @@ import Icon from './Icon';
 import setlife from '../scripts/setlife'
 
 export default class LandingPage extends Base {
-    handleStartCoding() {
-        setlife.clearLandingPage();
-    }
     render() {
         return (
             <div className='LandingPage'>
                 <p>This is the LandingPage component</p>
-                
-                
-                
             </div>
         );
     }
